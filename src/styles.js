04c55e6d7c0fb4b9c44aba8663d6353829e70074
Braimah-Abiola <braimahabiola.official@gmail.com>
2023-03-09 12:52:19 +0000

    Added Esteban Font

--- src/styles.js
+++ src/styles.js
@@ -2,16 +2,12 @@ const styles = {
   paddingX: "sm:px-16 px-6",
   paddingY: "sm:py-16 py-6",
   padding: "sm:px-16 px-6 sm:py-16 py-10",
+  padding2: "sm:px-12 px-4 sm:py-8 py-2",
 
   heading:
     "font-bold xs:text-[48px] text-[40px] text-nav xs:leading-[76.8px] leading-[66.8px] w-full",
   heading4:
     "font-bold font-semibold xs:text-[24px] text-[18px] text-nav w-full",
-
-  heroHeadText:
-    "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
-  heroSubText:
-    "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
 
   sectionHeadText:
     "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
@@ -21,8 +17,7 @@ const styles = {
   flexCenter: "flex justify-center items-center",
   flexStart: "flex justify-center items-start",
 
-  paragraph: "font-normal text-[20px]",
-  padding2: "sm:px-12 px-4 sm:py-8 py-2",
+  paragraph: "text-[20px]",
 };
 
 export { styles };
